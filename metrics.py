beb0058f750812cb8ffc62816a698d302f8fc284
Olamide Olanrewaju <85569620+Olamiknight@users.noreply.github.com>
2025-05-14 16:38:16 -0400

    Adding metrics.py file

---
+++ metrics.py
@@ -0,0 +1,34 @@
+
+#Compute mutual information, cross correlation, mse between the fixed regression and moving tbet images 
+
+def compute_metrics(fixed_image, moving_image):
+
+    # Compute Mutual Information
+    mutual_info = ants.image_mutual_information(fixed_image, moving_image)
+
+    # Compute Cross Correlation
+    cross_corr = ants.math.image_similarity(fixed_image, moving_image,'Correlation')
+
+    # Compute Mean Squared Error
+    mse = ants.math.image_similarity(fixed_image, moving_image,'MeanSquares')
+
+    #Binarize the images
+    fixed_image_bin = (fixed_image.numpy()> 0.5).astype(int)
+    moving_image_bin =  (moving_image.numpy() > 0.5).astype(int)
+    # Compute Jaccard Index
+    jaccard_index = jaccard_score(fixed_image_bin.flatten(), moving_image_bin.flatten(), average='weighted')
+    
+    #Compute the Dice coefficient
+    f1 = f1_score(fixed_image_bin.flatten(), moving_image_bin.flatten(), average='weighted')
+
+    # Get image from array
+    fixed_image = sitk.GetImageFromArray(fixed_image.numpy())
+    moving_image = sitk.GetImageFromArray(moving_image.numpy())
+
+    #Compute the Hausdorff distance
+    hausdoroff_distance = sitk.HausdorffDistanceImageFilter()
+    hausdoroff_distance.Execute(fixed_image, moving_image)
+    hausdoroff_distance_value = hausdoroff_distance.GetHausdorffDistance()
+
+    return mutual_info, cross_corr, mse, jaccard_index, f1, hausdoroff_distance_value
+
---
+++ padding.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import SimpleITK as sitk
+import ants
+from sklearn.metrics import jaccard_score, f1_score
+
+def pad_images_to_same_size(images):
+    # Get the maximum dimensions
+    max_shape = np.max([img.shape for img in images.values()], axis=0)
+
+    # Pad each image to the maximum dimensions
+    padded_images = {}
+    for sampleid, img in images.items():
+        pad_width = [(0, max_dim - img_dim) for img_dim, max_dim in zip(img.shape, max_shape)]
+        padded_images[sampleid] = np.pad(img, pad_width, mode='constant', constant_values=0)
+    
+    return padded_images
+
